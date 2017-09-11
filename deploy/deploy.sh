c58e1f95a4c526bc0e396e6279a29c39a9ad6ef4
erodriguez <eduardo@rodriguezcarro.es>
2017-09-11 20:58:49 +0200

    ANSI encoding & LF

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 
- if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&  [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
- 
-    echo "Deploy $TRAVIS_TAG"
-    
-    echo "Preparing key"
-    openssl aes-256-cbc -K $encrypted_3d7f448369a7_key -iv $encrypted_3d7f448369a7_iv -in deploy/signingkey.asc.enc -out deploy/signingkey.asc -d
-    
-    echo "Importing key"
-    gpg --fast-import deploy/signingkey.asc
-    
-    echo "Maven deploy"
-    mvn deploy -P sign,build-extras --settings deploy/settings.xml
-    
+if ([ "$TRAVIS_BRANCH" == "master" ] || [ ! -z "$TRAVIS_TAG" ]) &&  [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+	echo "Deploy $TRAVIS_TAG"
+	echo "Preparing key"
+	openssl aes-256-cbc -K $encrypted_3d7f448369a7_key -iv $encrypted_3d7f448369a7_iv -in deploy/signingkey.asc.enc -out deploy/signingkey.asc -d
+	echo "Importing key"
+	gpg --fast-import deploy/signingkey.asc
+	echo "Maven deploy"
+	mvn deploy -P sign,build-extras --settings deploy/settings.xml
  else
-    echo "Not for deploy"
- fi
- 
+	echo "Not for deploy"
+fi
+
